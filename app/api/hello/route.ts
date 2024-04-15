fe1662e8d8caa280c4e35186a6491cfa104b561e
Turner Bohlen <turnerbohlen@gmail.com>
2024-04-14 22:49:18 -0700

    Clears repo to switch to nuxt

--- app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
-
-export function GET(request: Request) {
-  return new Response(`Hello again from ${process.env.VERCEL_REGION}`);
-}
-
-export const runtime = 'nodejs';
