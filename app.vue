0a8a5a151bdb824fc52a094a17d39705c565e109
Turner Bohlen <turnerbohlen@gmail.com>
2024-04-14 23:45:26 -0700

    Adds supabase

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://wbqhbuxxzypoakzmslxi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndicWhidXh4enlwb2Frem1zbHhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMwNTA2NjAsImV4cCI6MjAyODYyNjY2MH0.T_LPqo5YvJlr9AdDCcv2l71ERZiuLYZq2NdBH3YOEXM')
+const learningExperiences = ref([])
+
+async function getLearingExperiences() {
+  const { data } = await supabase.from('learningExperiences').select()
+  learningExperiences.value = data
+}
+
+onMounted(() => {
+  getLearingExperiences()
+})
+</script>
+
+
 <template>
   <div>
-    <NuxtWelcome />
+    <li v-for="experience in learningExperiences" :key="experience.id">{{ experience.subject }}</li> 
   </div>
 </template>
